cbe161d0dc10ce8adb9da458febf1d8efbe94654
mishraprince0786@gmail.com <mishraprince0786@gmail.com>
2018-12-14 09:21:26 +0530

    adding CORS

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 
+
+// Cross origin resourse sharing config
+app.use(function (request, response, next) {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
