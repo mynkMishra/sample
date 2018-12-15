8bc3fa59c07efa68e06c70dd333bd652597a9b2f
mishraprince0786@gmail.com <mishraprince0786@gmail.com>
2018-12-15 22:42:55 +0530

    setting mail

--- config/utils/mailer.js
+++ config/utils/mailer.js
@@ -5,15 +5,15 @@ const mailer = {
     sendEmail : function(email){
         var {to, subject, emailText } = email 
         var transporter = nodemailer.createTransport({
-          service : config.SMTP_SERVICE,
+          service : config.SMTP_SERVICE || process.env.SMTP_SERVICE,
           auth : {
-              user : config.SMTP_MAIL_ID,
-              pass : config.SMTP_MAIL_PASS
+              user : config.SMTP_MAIL_ID || process.env.SMTP_MAIL_ID,
+              pass : config.SMTP_MAIL_PASS || process.env.SMTP_MAIL_PASS
           }
         });
 
         var mailOptions = {
-            from : config.COMPANY_NAME + '<h=' + config.SMTP_MAIL_ID+'>',
+            from : config.COMPANY_NAME || process.env.COMPANY_NAME + '<h=' + config.SMTP_MAIL_ID ||process.env.SMTP_MAIL_ID+'>',
             to : to, //sending to user email
             subject: subject, // Subject line
             text: emailText, // plaintext body
